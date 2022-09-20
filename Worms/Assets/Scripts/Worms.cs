cd31b49b9e9fa73cbb46044fdb90913cf4cd3439
Grengidone <wezeman.arthur@gmail.com>
2022-09-20 15:47:02 +0200

    First Push

---
+++ Worms/Assets/Scripts/Worms.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Worms
+{
+    public Worms(int id)
+    {
+        _id = id;
+    }
+    private int _health;
+    private string _name;
+    private int _id;
+
+}
---
+++ Worms/Assets/Scripts/WormsController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IDamageable
+{
+
+}
+public class WormsController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+
